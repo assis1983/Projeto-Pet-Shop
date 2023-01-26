1cf17a9e6e0ccf35721fc2c674c19a8cef9206f5
Eder Assis <109834675+assis1983@users.noreply.github.com>
2023-01-26 19:17:49 -0300

    Cria o conjunto de visualização

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from rest_api.views import hello_world
+from rest_framework.routers import SimpleRouter
+from rest_api.views import AgendamentoModelViewSet, hello_world
+
 
 
 app_name = 'rest_api'
+
+router = SimpleRouter(trailing_slash=False)
+router.register('agendamento', AgendamentoModelViewSet)
+
 urlpatterns = [
     path('hello_world', hello_world, name='hello_world_api'),
 ]
 
+urlpatterns += router.urls
